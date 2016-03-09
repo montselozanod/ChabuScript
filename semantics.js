c5cb19249b27821068e1c9be75e448b68140ef85
montselozanod <montse_967@hotmail.com>
2016-03-09 13:00:19 -0600

    add global and local variables

--- semantics.js
+++ semantics.js
@@ -8,7 +8,16 @@ var current = {
 
 //directory of all procedures in a program
 var dirProcs = {};
+/*
+  nombre,
+  tipo,
+  variable = referencia a la tabla de variables del scope
+*/
 
+/* tabla de variables
+  nombre
+  tipo
+*/
 //global variables
 var globalVars = {};
 
@@ -20,4 +29,20 @@ function addProc(name, procedure)
 {
   console.log("add " + name + " to procs directory.");
   dirProcs[name] = procedure;
+}
+
+function addLocalVar(id, type)
+{
+  localVars[id] = {
+    'id': id,
+    'type': type,
+  }
+}
+
+function addGlobalVar(id, type)
+{
+  globalVars[id] = {
+    'id': id,
+    'type': type,
+  }
 }
