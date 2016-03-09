2af702e295fd1d6001b9bca36a58f5dd2eac5065
montselozanod <montse_967@hotmail.com>
2016-03-09 17:59:17 -0600

    semantic errors

--- semantics.js
+++ semantics.js
@@ -1,3 +1,11 @@
+var errors{
+  'PARAMETER_TYPE_MISMATCH': 'Function {0} expects type {1} and received type {2} in position {3}',
+  'PARAMETER_LENGTH_MISMATCH': 'Function {0} expects {1} parameters and is invoked with {2}',
+  'UNDECLARED_VARIABLE': 'Undeclared variable {0} found',
+  'UNDECLARED_FUNCTION': 'Undeclared function {0} found',
+  'DUPLICATE_VARIABLE_NAME': 'Duplicate variable name {0} found',
+  'DUPLICATE_FUNCTION_NAME': 'Duplicate function name {0} found',
+}
 
 //current function
 var current = {
@@ -5,6 +13,7 @@ var current = {
   'id': 'start',
   'params':[],
 };
+
 
 //directory of all procedures in a program
 var dirProcs = {};
@@ -44,5 +53,15 @@ function addGlobalVar(id, type)
   globalVars[id] = {
     'id': id,
     'type': type,
+  }
+}
+
+function varIsUnique(id)
+{
+  //.hasOwnProperty(id)
+  if(!(id in current[vars]))
+    return false;
+  else {
+    return true;
   }
 }
