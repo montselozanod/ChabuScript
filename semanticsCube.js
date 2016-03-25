8b9c2a323a3bb49fef38f77b63346545d82c9ad0
montselozanod <montse_967@hotmail.com>
2016-03-25 15:02:10 -0600

    semantics cube operations and types

---
+++ semanticsCube.js
@@ -0,0 +1,25 @@
+//Operation Types
+
+var Type: {
+  NUMBER: 1,
+  STRING: 2,
+  BOOL : 3,
+};
+
+var Operation: {
+  MULT: 1,
+  DIV: 2,
+  SUM: 3,
+  MINUS: 4,
+  AND: 5,
+  OR: 6,
+  NOT: 7,
+  LESS: 8,
+  GRT: 9,
+  EQL: 10,
+  DIFF: 11,
+  ERR: 12
+};
+
+// Operations
+var semantic_cube = [];
