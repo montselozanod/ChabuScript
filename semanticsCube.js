c3764c77ab7309b175a175d4a7617daef3c3a9b8
montselozanod <montse_967@hotmail.com>
2016-03-25 22:22:02 -0600

    initializing semantic cube

--- semanticsCube.js
+++ semanticsCube.js
@@ -23,3 +23,32 @@ var Operation: {
 
 // Operations
 var semanticCube = [];
+semanticCube[Type.NUMBER] = [];
+semanticCube[Type.STRING] = [];
+semanticCube[Type.BOOL] = [];
+
+semanticCube[Type.NUMBER][Type.NUMBER] = [];
+semanticCube[Type.NUMBER][Type.STRING] = [];
+semanticCube[Type.NUMBER][Type.BOOL] = [];
+
+semanticCube[Type.STRING][Type.NUMBER] = [];
+semanticCube[Type.STRING][Type.STRING] = [];
+semanticCube[Type.STRING][Type.BOOL] = [];
+
+semanticCube[Type.BOOL][Type.NUMBER] = [];
+semanticCube[Type.BOOL][Type.STRING] = [];
+semanticCube[Type.BOOL][Type.BOOL] = [];
+
+//INITIALIZE SEMANTIC CUBE
+
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.MULT] = Type.NUMBER;
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.DIV] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.SUM] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.MINUS] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.AND] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.OR] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.NOT] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.LESS] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.GRT] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.EQL] = [];
+semanticCube[Type.NUMBER][Type.NUMBER][Operation.DIFF] = [];
