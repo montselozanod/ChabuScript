f4d203dc5c34249e26732b5f2f64c21feec9405e
montselozanod <montse_967@hotmail.com>
2016-03-31 19:24:33 -0600

    quadcont

--- init.js
+++ init.js
@@ -1,1 +1,12 @@
 var quadruples = {};
+var dirProcs = {};
+var varTable = {};
+var quadCont = 0;
+var errors{
+  'PARAMETER_TYPE_MISMATCH': 'Function {0} expects type {1} and received type {2} in position {3}',
+  'PARAMETER_LENGTH_MISMATCH': 'Function {0} expects {1} parameters and is invoked with {2}',
+  'UNDECLARED_VARIABLE': 'Undeclared variable {0} found',
+  'UNDECLARED_FUNCTION': 'Undeclared function {0} found',
+  'DUPLICATE_VARIABLE_NAME': 'Duplicate variable name {0} found',
+  'DUPLICATE_FUNCTION_NAME': 'Duplicate function name {0} found',
+}
