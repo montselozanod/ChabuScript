e3f8b6b4ccd60def0b687df60462c3a10a2d8428
montselozanod <montse_967@hotmail.com>
2016-04-13 23:36:14 -0500

    random

---
+++ paintCanvas.js
@@ -0,0 +1,36 @@
+var canvas = document.getElementById('canvas');
+
+function cleanCanvas()
+{
+
+}
+
+function setBackground(r, g, b)
+{
+
+}
+
+function drawLine()
+{
+
+}
+
+function drawCircle()
+{
+
+}
+
+function drawRectangle()
+{
+
+}
+
+function drawPolygon()
+{
+
+}
+
+function generateRandom(min, max)
+{
+  return Math.random() * (max - min) + min;
+}
