339e0e7b213ad94688601dcf23a0cdd81f0a6474
montselozanod <montse_967@hotmail.com>
2016-04-14 16:58:38 -0500

    fix bug in enums2

--- init.js
+++ init.js
@@ -7,7 +7,7 @@ var constants = {};
 var numVars = 0;
 var stringVars = 0;
 var boolVars = 0;
-var errors{
+var errors = {
   'PARAMETER_TYPE_MISMATCH': 'Function {0} expects type {1} and received type {2} in position {3}',
   'PARAMETER_LENGTH_MISMATCH': 'Function {0} expects {1} parameters and is invoked with {2}',
   'UNDECLARED_VARIABLE': 'Undeclared variable {0} found',
